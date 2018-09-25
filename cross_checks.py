686d29d083f92e64396454fd7acdb49c5b7a14e1
b282022 <superm15@protonmail.com>
2018-09-25 13:36:21 +0530

    Update subroutine to getting the cross checks

--- cross_checks.py
+++ cross_checks.py
@@ -1,48 +1,23 @@
-from utility import find_word_above, find_word_below, common_letters, all_letters, read_dictionary
+from utility import find_word_above, find_word_below, all_letters, binary_search
+from utility import read_dictionary
 
 
-def cross_checks_below(board, row, dictionary):
-    valid_letters_below = [[] for _ in range(len(board[row]))]
+def cross_checks(board, row, dictionary):
+    valid_letters = [[] for _ in range(len(board[row]))]
     for col in range(len(board[row])):
         if board[row][col] != '-':
             continue
         word_below = find_word_below(board, row, col)
-        if word_below == '':
-            valid_letters_below[col] = all_letters()
-        else:
-            for word in dictionary:
-                if word[1:] == word_below \
-                    and word[0] not in valid_letters_below[col]:
-                    valid_letters_below[col].append(word[0])
-    return valid_letters_below
-    
-
-def cross_checks_above(board, row, dictionary):
-    valid_letters_above = [[] for _ in range(len(board[row]))]
-    for col in range(len(board[row])):
-        if board[row][col] != '-':
-            continue
         word_above = find_word_above(board, row, col)
-        if word_above == '':
-            valid_letters_above[col] = all_letters()
-        else: 
-            for word in dictionary:
-                if word.startswith(word_above) \
-                and len(word) == len(word_above) + 1 \
-                and word[-1] not in valid_letters_above[col]:
-                    valid_letters_above[col].append(word[-1])
-    return valid_letters_above
-
-
-def cross_checks(board, row, dictionary):
-    if row == 0:
-        return cross_checks_below(board, row, dictionary)
-    if row == len(board) - 1:
-        return cross_checks_above(board, row, dictionary)
-    valid_letters_below = cross_checks_below(board, row, dictionary)
-    valid_letters_above = cross_checks_above(board, row, dictionary)
-    return [common_letters(valid_letters_above[i], valid_letters_below[i]) \
-        for i in range(len(valid_letters_above))]
+        if word_above == '' and word_below == '':
+            valid_letters[col] = all_letters()
+            continue
+        for i in range(ord('a'), ord('z') + 1):
+            whole_word = word_above + chr(i) + word_below
+            if binary_search(dictionary, whole_word) == -1:
+                continue
+            valid_letters[col].append(chr(i))
+    return valid_letters
 
 
 if __name__ == '__main__':
@@ -50,8 +25,8 @@ if __name__ == '__main__':
         ['-', '-', 'a', '-', '-'],
         ['-', '-', 't', '-', '-'],
         ['-', '-', '-', '-', '-'],
-        ['-', '-', 'a', 'z', 't'],
-        ['-', '-', 'r', 'z', '-'],
+        ['-', '-', 'a', 'i', 't'],
+        ['-', '-', 'r', 'm', '-'],
         ['-', '-', 't', '-', '-']
     ]
-    print cross_checks(board, 2, read_dictionary())[2]
+    print cross_checks(board, len(board) - 1, read_dictionary())
